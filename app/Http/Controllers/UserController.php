d3d65e6e15520ceca99815eb16e62717c93cd027
Wisang Garnies <garniesw@gmail.com>
2024-03-13 21:15:22 +0700

    Pertemuan 4_Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,8 @@ class UserController extends Controller
 {
     //
     public function index(){
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id',2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
